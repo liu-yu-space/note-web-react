a44dab098ea6bcc5155e517a89d780eaa07b43d3
LiuYu <2470809972@qq.com>
2025-03-10 23:36:46 +0800

    feat: 添加lucide

--- README.md
+++ README.md
@@ -26,4 +26,6 @@ pnpm dev
 
 ## 其他
 
-### 关于提交:使用Husky、Commitlint 和 Lint-Staged来规范提交文件和提交信息：规范[https://www.conventionalcommits.org/]
+### 关于提交:使用Husky、Commitlint 和 Lint-Staged来规范提交文件和提交信息：[conventionalcommits](https://www.conventionalcommits.org/)
+
+### 关于icon 引入了[Lucide](src/pages/login/login.tsx)
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,20 +1,35 @@
+import { useNavigate } from "react-router-dom";
+import { Lock, User } from "lucide-react";
+
 export default function Home() {
+    const navigate = useNavigate();
+    const linlB = () => {
+        // 直接跟我们定义的path
+        void navigate("/");
+    };
     return (
-        <section className="text-gray-900 h-dvh flex justify-center items-center bg-[url(@/assets/login-bg1.png)] bg-cover bg-center">
+        <section className="h-dvh flex justify-center items-center bg-[url(@/assets/login-bg1.png)] bg-cover bg-center">
             <div className="bg-white/80 p-8 rounded-lg shadow-lg h-100 w-180">
+                <img src="" alt="" />
                 <h1 className="text-2xl display-flex justify-center align-middle">
                     学而时习之,不亦说乎！
                 </h1>
                 <p className="text-sm flex flex-col items-center mt-15">
                     <label htmlFor="" className="block mt-4 border-b border-gray-300 w-90">
-                        <span>用户名：</span>
+                        <User className="w-6 h-6 text-(--color-main)" />
                         <input type="text" />
                     </label>
                     <label htmlFor="" className="block mt-4 border-b border-gray-300 w-90">
-                        <span className="w-14 inline-flex justify-end">密码：</span>
+                        <Lock className="w-6 h-6 text-(--color-main)" />
                         <input type="password" />
                     </label>
-                    <button className="border rounded-[4px] px-4 py-1 text-sm mt-10">登 录</button>
+                    <button
+                        type="button"
+                        className="border rounded-[4px] px-4 py-1 text-sm mt-10"
+                        onClick={linlB}
+                    >
+                        登 录
+                    </button>
                 </p>
             </div>
         </section>
