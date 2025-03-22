2787dc5cf0da4724863ce2d208f01b6bef1c3334
LiuYu <2470809972@qq.com>
2025-03-22 18:34:50 +0800

    feat: 调整ts规范检查

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import Home from "./pages/home/home.tsx";
 import Note from "./pages/note/note.tsx";
 import Demo from "./pages/demo/index.tsx";
 import Aboutme from "./pages/aboutme/index.tsx";
-// import Navbar from "./pages/layout/nav.tsx";
+import Navbar from "./pages/layout/nav.tsx";
 
 function App() {
     return (
-        <main className="w-dvw h-dvh">
-            {/* <Navbar /> */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/note" element={<Note />} />
-                <Route path="/demo" element={<Demo />} />
-                <Route path="/aboutme" element={<Aboutme />} />
-            </Routes>
-        </main>
+        <div className="w-dvw h-dvh flex flex-col">
+            <Navbar />
+            <div className="grow">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/note" element={<Note />} />
+                    <Route path="/demo" element={<Demo />} />
+                    <Route path="/aboutme" element={<Aboutme />} />
+                </Routes>
+            </div>
+        </div>
     );
 }
 
--- src/pages/note/note.tsx
+++ src/pages/note/note.tsx
@@ -1,34 +1,44 @@
-import markdownit from 'markdown-it';
-import { FormEvent, useState } from 'react'; // 导入 React 和 FormEvent 类型
+import markdownit from "markdown-it";
+import { FormEvent, useState } from "react";
+import { WButton } from "@/components";
 
 export default function Note() {
-
     const md = markdownit();
-    // const result = md.render('# markdown-it rulezz!');
-    const [mdText, setMdText] = useState('');
- 
+    const [mdText, setMdText] = useState("");
+
     function handleInput(event: FormEvent<HTMLDivElement>) {
         const input = event.target as HTMLInputElement;
         console.log(input.innerHTML);
-        if(input?.innerHTML) {
+        if (input?.innerHTML) {
             const output = md.render(input.innerHTML);
             console.log(output);
             setMdText(output);
         }
     }
 
-    return <main>
-        <div>
-            <pre className="font-lxgw">
-                凉州词之二（王之涣）<br></br>
-            黄河远上白云间，一片孤城万仞山。<br></br>
-            羌笛何须怨杨柳，春风不度玉门关。<br></br>
-            </pre>
+    return (
+        <div className="flex flex-col w-full h-full">
+            <div className="flex w-full h-12 border-r border-gray-200 py-3 px-10">
+                <input
+                    className="w-full text-base outline-none"
+                    type="text"
+                    placeholder="请输入标题"
+                />
+                <div className="flex w-20 direction-row justify-end items-center">
+                    <WButton type="text">保存</WButton>
+                </div>
+            </div>
+            <div className="flex gap-2 w-full h-full pb-4">
+                <div
+                    contentEditable={"plaintext-only"}
+                    className="w-full h-full border border-gray-200 outline-none p-4"
+                    onInput={handleInput}
+                ></div>
+                <div
+                    className="w-full h-[100%] border font-lxgw md-container border-gray-200 outline-none p-4"
+                    dangerouslySetInnerHTML={{ __html: mdText }}
+                ></div>
+            </div>
         </div>
-        
-        <div className='flex gap-4'>
-            <div contentEditable={'plaintext-only'} className='w-100 h-100 border' onInput={handleInput}></div>
-            <div className='w-100 h-100 border font-lxgw' dangerouslySetInnerHTML={{ __html: mdText }}></div>
-        </div>
-    </main>;
+    );
 }
